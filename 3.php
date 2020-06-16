b8a637b998d6205dbf99c79ff071849b10a67b33
Eduard Ilyin <eduardilgen@gmail.com>
2020-06-16 05:18:07 +0200

    New

--- 3.php
+++ 3.php
@@ -1,44 +1,14 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<form method="get">
-    <input type="text" name="input">
-</form>
-</body>
-</html>
 <?php
 
-if (!empty($_GET['input'])){
-    if (correctBrackets($_GET['input'])){
-        echo "Правлиьно";
-    } else echo "Не правильно";
-}
-
-function correctBrackets($input){
-    $input = preg_replace('/[^(){}\[\]\'\"`]/','',$input);
-    echo $input . "<br>";
-    $brackets = str_split($input);
-    $stack = new SplStack();
-    $map = [']' => '[', ')' => '(', '}' => '{'];
-    $closing = array_keys($map);
-    foreach ($brackets as $bracket){
-        if (!in_array($bracket,$closing)){
-            $stack->push($bracket);
-        } else{
-            if ($stack->isEmpty()){
-                return false;
-            }
-            $stack->pop();
-        }
+function render($cols,$rows){
+    $beginCol = 1;
+    $beginRow = 1;
+    $endCol = $cols;
+    $endRow = $rows;
+    $array = [];
+    while ($beginCol <= $endCol || $beginRow <= $endRow){
+        if ()
     }
-    return $stack->isEmpty();
 }
 
 
